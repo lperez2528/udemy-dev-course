0e947c438b574e7aa6341cd3d2eec899e0b45876
Luis Perez <lperez2528@gmail.com>
2018-02-06 15:03:12 -0600

    Almost done

--- scoreKeeper/scoreKeeper.js
+++ scoreKeeper/scoreKeeper.js
@@ -15,6 +15,7 @@ p1Button.addEventListener("click", function() {
     p1Score++;
     if (p1Score === winningScore) {
       gameOver = true
+      p1Display.classList.add("winner")
     }
     p1Display.textContent = p1Score;
   } 
@@ -25,11 +26,19 @@ p2Button.addEventListener("click", function() {
     p2Score++;
     if (p2Score === winningScore) {
       gameOver = true
+      p2Display.classList.add("winner")
+
     }
     p2Display.textContent = p2Score;
   }
 });
 
 reset.addEventListener("click", function() {
+  p1Score = 0
+  p2Score = 0
+  p1Display.textContent = 0
+  p2Display.textContent = 0
+  p1Display.classList.remove("winner")
+  p2Display.classList.remove("winner")
   gameOver = false
 })
