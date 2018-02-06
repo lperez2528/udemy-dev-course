0af53c5e273763520e31e72d35fa31daf3e6fe76
Luis Perez <lperez2528@gmail.com>
2018-02-06 14:52:37 -0600

    scoreKeeper update

--- scoreKeeper/scoreKeeper.js
+++ scoreKeeper/scoreKeeper.js
@@ -4,16 +4,32 @@ var p1Display = document.querySelector("#p1Display")
 var p2Display = document.querySelector("#p2Display")
 var p1Score = 0
 var p2Score = 0
+var gameOver = false
+var winningScore = 5
+// var winningScore = document.querySelector("#winningScore")
 
 var reset = document.querySelector("#reset")
 
-p1Button.addEventListener("click", function(){
-  p1Score++;
-  p1Display.textContent = p1Score;
+p1Button.addEventListener("click", function() {
+  if (!gameOver) {
+    p1Score++;
+    if (p1Score === winningScore) {
+      gameOver = true
+    }
+    p1Display.textContent = p1Score;
+  } 
 });
 
-p2Button.addEventListener("click", function(){
-  p2Score++;
-  p2Display.textContent = p2Score;
+p2Button.addEventListener("click", function() {
+  if (!gameOver) {
+    p2Score++;
+    if (p2Score === winningScore) {
+      gameOver = true
+    }
+    p2Display.textContent = p2Score;
+  }
 });
 
+reset.addEventListener("click", function() {
+  gameOver = false
+})
